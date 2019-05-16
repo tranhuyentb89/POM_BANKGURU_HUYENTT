38d0c8b437637fb58d4ae53de3e576ff0f15b8a4
ACER <ACER@192.168.1.9>
2019-05-17 01:19:52 +0700

    POM 6 Processing

---
+++ testcases/com/bankguru/account/Account_Level_06_MultiBrowser_ParallelTesting.java
@@ -0,0 +1,96 @@
+package com.bankguru.account;
+
+import org.testng.annotations.Test;
+
+import commons.PageFactoryManage;
+import pageObjects.HomePageObject;
+import pageObjects.LoginPageObject;
+import pageObjects.RegisterPageObject;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+
+public class Account_Level_06_MultiBrowser_ParallelTesting {
+	WebDriver driver;
+	private String email, userID, password, loginPageUrl;
+	LoginPageObject loginPage;
+	RegisterPageObject registerPage;
+	HomePageObject homePage;
+
+	@Parameters("browser")
+	@BeforeClass
+	public void beforeClass(String browserName) {
+		if (browserName.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+		} else if (browserName.equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver", ".\\resources\\chromedriver.exe");
+			driver = new ChromeDriver();
+		} else if (browserName.equalsIgnoreCase("chromeheadless")) {
+			System.setProperty("webdriver.chrome.driver", ".\\resources\\chromedriver.exe");
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("window-size=1366x768");
+			options.addArguments("headless");
+			driver = new ChromeDriver();
+		}
+		driver.get("http://demo.guru99.com/v4/");
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		email = "tranhuyentb89" + ramdomNumber() + "@gmail.com";
+		// loginPage = new LoginPageObject(driver);
+		loginPage = PageFactoryManage.getLoginPage(driver);
+	}
+
+	@Test
+	public void TC_01_Register() {
+		// khoi tạo login page để map driver giữa 2 tầng
+		// loginPage = new LoginPageObject(driver);
+		Assert.assertTrue(loginPage.isLoginFormDisplayed());
+		loginPageUrl = loginPage.getLoginPageUrl();
+		registerPage = loginPage.clickToHereLink();
+		Assert.assertTrue(registerPage.isRegisterPageDisplayed());
+		registerPage.inputToEmailTextbox(email);
+		registerPage.clickToLoginButton();
+		userID = registerPage.getUserIDInfo();
+		password = registerPage.getPasswordInfo();
+	}
+
+	@Test
+	public void TC_02_LoginWithAboveInfo() {
+		loginPage = registerPage.openLoginPage(loginPageUrl);
+		// loginPage = new LoginPageObject(driver);
+		Assert.assertTrue(loginPage.isLoginFormDisplayed());
+		loginPage.inputToUserIDTextbox(userID);
+		loginPage.inputToPasswordTextbox(password);
+		homePage = loginPage.clickToLoginButton();
+
+		Assert.assertTrue(homePage.isWelcomeMsgDisplayed());
+		Assert.assertTrue(homePage.isUserIDisplayed(userID));
+
+		// Logout
+		loginPage = homePage.clickToLougoutLink();
+		Assert.assertTrue(loginPage.isLoginFormDisplayed());
+
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+	public int ramdomNumber() {
+		Random radom = new Random();
+		int number = radom.nextInt(9999999);
+		return number;
+	}
+
+}
