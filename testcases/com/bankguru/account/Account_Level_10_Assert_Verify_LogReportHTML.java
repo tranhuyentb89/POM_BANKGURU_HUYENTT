d5023d5ddc4780f8cf4357ee071395b5218823be
ACER <ACER@192.168.1.3>
2019-05-31 00:49:05 +0700

    aaaa

--- testcases/com/bankguru/account/Account_Level_10_Assert_Verify_LogReportHTML.java
+++ testcases/com/bankguru/account/Account_Level_10_Assert_Verify_LogReportHTML.java
@@ -3,6 +3,7 @@ package com.bankguru.account;
 import java.util.Random;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
@@ -39,32 +40,48 @@ public class Account_Level_10_Assert_Verify_LogReportHTML extends AbstractTest {
 
 	@Test
 	public void TC_01_Register() {
-		// khoi tạo login page để map driver giữa 2 tầng
-		// loginPage = new LoginPageObject(driver);
+		log.info("Register - Step 01: Verify Login form displayed");
 		verifyTrue(loginPage.isLoginFormDisplayed());
+		
+		log.info("Register - Step 02: Get URL of Login form");
 		loginPageUrl = loginPage.getLoginPageUrl();
+		
+		log.info("Register - Step 03: Click to here link");
 		registerPage = loginPage.clickToHereLink();
+		
+		log.info("Register - Step 04: Verify Register page is displayed");
 		verifyFalse(registerPage.isRegisterPageDisplayed());
+		
+		log.info("Register - Step 05: Input email and click to Login button");
 		registerPage.inputToEmailTextbox(email);
 		registerPage.clickToLoginButton();
+		
+		log.info("Register - Step 06: Get username and password");
 		userID = registerPage.getUserIDInfo();
 		password = registerPage.getPasswordInfo();
 	}
 
 	@Test
 	public void TC_02_LoginWithAboveInfo() {
+		log.info("Login - Step 01: Open login form");
 		loginPage = registerPage.openLoginPage(loginPageUrl);
 		// loginPage = new LoginPageObject(driver);
+		
+		log.info("Login - Step 02: Verify login form dislayed");
 		verifyTrue(loginPage.isLoginFormDisplayed());
+		
+		log.info("Login - Step 03: Input username and password");
 		loginPage.inputToUserIDTextbox(userID);
 		loginPage.inputToPasswordTextbox(password);
+		
+		log.info("Login - Step 04: Click to login button");
 		homePage = loginPage.clickToLoginButton();
 
-		boolean homePageStatus = homePage.isWelcomeMsgDisplayed();
-		System.out.println("Home Page status =" + homePageStatus);
+		log.info("Login - Step 05: Verify welcome message displayed");
+		verifyTrue(homePage.isWelcomeMsgDisplayed());
 		
-		boolean loginFormStatus = homePage.isLoginFormUndisplayed();
-		System.out.println("Login form status =" + loginFormStatus);
+		log.info("Login - Step 06: Verify login form displayed");
+		verifyTrue(homePage.isLoginFormUndisplayed());
 		
 		// Logout
 		// loginPage = homePage.clickToLougoutLink();
